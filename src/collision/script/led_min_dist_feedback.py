adcaf59a39409560df55b6a6c8fbaa3edb3f42de
William Bénichou <benichou.william@outlook.fr>
2020-11-22 16:27:46 +0100

    Untested notice

--- src/collision/script/led_min_dist_feedback.py
+++ src/collision/script/led_min_dist_feedback.py
@@ -4,8 +4,10 @@ import time
 from std_msgs.msg import Empty
 from kobuki_msgs.msg import Led
 
+# IMPORTANT: Ce code n'a pas été testé car réalisé or des heures de TP (sans turtlebot à disposition)
+
 class BlinkRadar:
-    blinkDuration = 0.05
+    blinkDuration = 0.03 #blink faster than maximum frequency (Support 33Hz max)
     def __init__(self):
         rospy.init_node('listener', anonymous=True)
         rospy.Subscriber("/distance_warning_blink", Empty, self.callback)
@@ -14,11 +16,13 @@ class BlinkRadar:
     def callback(self, data):
         #should trigger led
         ledmsg = Led()
-        ledmsg.value = ledmsg
+        ledmsg.value = ledmsg.GREEN
+        self.pub_led.publish(ledmsg)
 
-        
-def listener(callback):
-    
+        time.sleep(self.blinkDuration)
+
+        ledmsg.value = ledmsg.BLACK
+        self.pub_led.publish(ledmsg)
 
 if __name__ == '__main__':
     radar = BlinkRadar()
